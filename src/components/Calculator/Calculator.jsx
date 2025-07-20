30c411f7d1538db77e6bc6e42efd22578bb0c594
koch ko <ReifelG@yandex.ru>
2025-07-20 20:09:48 +0300

    feat: make DEL button remove entire function names at once

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -13,7 +13,15 @@ function Calculator() {
 				setInputValue('')
 				setAnswer('')
 			} else {
-				setInputValue(prev => prev.slice(0, -1))
+				setInputValue(prev => {
+					const functions = ['sqrt', 'sin', 'cos', 'tan', 'log', 'ln']
+					for (let func of functions) {
+						if (prev.endsWith(func)) {
+							return prev.slice(0, -func.length)
+						}
+					}
+					return prev.slice(0, -1)
+				})
 			}
 		} else if (value === '=') {
 			try {
@@ -29,15 +37,37 @@ function Calculator() {
 					.replace(/log/g, 'Math.log10')
 					.replace(/ln/g, 'Math.log')
 					.replace(/e/g, 'Math.E')
+					.replace(/π/g, 'Math.PI')
+					.replace(/(\d+(?:\.\d+)?)!/g, 'fact($1)')
 
-				const result = eval(expression)
+				const fact = n => {
+					if (n < 0) return NaN
+					if (n === 0 || n === 1) return 1
+					let res = 1
+					for (let i = 2; i <= n; i++) res *= i
+					return res
+				}
+
+				const result = Function('fact', 'return ' + expression)(fact)
 				setAnswer(result.toString())
 			} catch {
 				setAnswer('Ошибка: некорректное выражение')
 			}
 		} else {
 			if (answer !== '') setAnswer('')
-			setInputValue(prev => prev + value)
+
+			if (value === 'π') {
+				setInputValue(prev => prev + 'π')
+			} else if (value === '!') {
+				setInputValue(prev => {
+					if (/([0-9)]$)/.test(prev)) {
+						return prev + '!'
+					}
+					return prev
+				})
+			} else {
+				setInputValue(prev => prev + value)
+			}
 		}
 	}
 
